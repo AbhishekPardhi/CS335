4f99b676153b00dc31fef7ad0bab2ebe07b38467
AbhishekPardhi <abhipardhi326@gmail.com>
2023-03-22 22:09:01 +0530

    acha

---
+++ milestone2/tests/acha.java
@@ -0,0 +1,17 @@
+public class ExampleExpression {
+    public static void main(String[] args) {
+      int x = 150;
+      int y = 150;
+  
+    //   if (x < 100 || x > 200 && x != y) {
+    //     System.out.println("Ankur chakka");
+    //   } else {
+    //     System.out.println("Manas chakka");
+    //   }
+      if (x < 100 || y < 200) {
+        System.out.println("Ankur chakka");
+      } else {
+        System.out.println("Manas chakka");
+      }
+    }
+  }
