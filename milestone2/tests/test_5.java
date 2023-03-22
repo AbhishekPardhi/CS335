18c817cd51cd2ddbfb6652dd1b9898cafc858878
awsoankur <ankurk20@iitk.ac.in>
2023-03-22 22:48:32 +0530

    more todo

--- milestone2/tests/test_5.java
+++ milestone2/tests/test_5.java
@@ -9,7 +9,8 @@ class Rectangle implements Polygon {
 
   // implementation of abstract method
   public void getArea(int length, int breadth) {
-    System.out.println("The area of the rectangle is " + (length * breadth));
+    // System.out.println("The area of the rectangle is " + (length * breadth));
+    length=1;
   }
 }
 
