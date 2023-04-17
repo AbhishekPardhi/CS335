094db169830f8c4ce163dfd29e58e11460457a37
AbhishekPardhi <abhipardhi326@gmail.com>
2023-04-18 00:55:03 +0530

    Boht complex h bhai

--- milestone4/src/x86_64/x86.s
+++ milestone4/src/x86_64/x86.s
@@ -2,5 +2,13 @@ Function:
 	push rbp
 	mov rbp, rsp
 	mov rdi, [ebp + 8]
+	mov 0[rbp], rdi
 	mov rsi, [ebp + 16]
+	mov -8[rbp], rsi
+	mov rdx, [ebp + 24]
+	mov -16[rbp], rdx
 	add rdi, rsi
+	mov rax, rdi
+	mov rdx, rax
+	mov rdx, rax
+	mov rdx, rsi
