6ee8d2723d97b66045bf4c64d9d91bde664de707
awsoankur <ankurk20@iitk.ac.in>
2023-04-20 23:56:26 +0530

    update test

--- milestone4/src/test.sh
+++ milestone4/src/test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 filename="$1"
 mydir="$(pwd)"
-./main --input="$filename"
+./main --input=test_"$filename".java
 /bin/python3 ${mydir}/x86_64/generator.py
 gcc -c ./output/x86.s -o ./output/x86.o
 gcc ./output/x86.o -o ./output/x86
